9a1db59b390f6a7ce0bbd3bcc10bad1a69f30483
CianaTerese <W0402146@campus.nscc.ca>
2024-11-20 10:29:49 -0400

    Update Logs.java

--- Logs.java
+++ Logs.java
@@ -8,9 +8,9 @@ public class Logs extends Sprite implements Runnable {
     private boolean movingLeft;  
     private JLabel logLabel;
     private Rectangle r;
-    
-    //Game Started
-    private boolean gameStarted = false; 
+    private Character1 character1;
+    private JLabel character1Label;
+    private GamePrep gamePrep;
     
 
     public Logs(int x, int y, int width, int height, String imageName, int speed, boolean movingLeft) {
@@ -19,17 +19,40 @@ public class Logs extends Sprite implements Runnable {
         this.width = width;
         this.height = height;
         this.speed = speed;
-        this.movingLeft = movingLeft;
+        this.movingLeft = movingLeft; 
+        this.logLabel = new JLabel();
         
-        r = new Rectangle(x, y, width, height);
-        
-        logLabel = new JLabel();
-        ImageIcon logIcon = new ImageIcon(getClass().getResource("/images/" + imageName));
-        Image logImage = logIcon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        logLabel.setIcon(new ImageIcon(logImage));
+        logLabel = new JLabel(new ImageIcon(getClass().getResource("images/" + imageName)));
         logLabel.setSize(width, height);
         logLabel.setLocation(x, y);
+        
+        this.r = new Rectangle(x, y, width, height);
     }
+    
+    public void setCharacter1(Character1 temp) {
+    	this.character1 = temp;
+    	this.character1Label = temp.getLabel();  
+	}
+    public void setcharacter1Label(JLabel temp) {
+		this.character1Label = temp;
+	}    
+	    
+	public JLabel getCharacter1Label() {
+		return character1Label; }
+	
+    public void startThread() {
+        Thread t = new Thread(this, "Log thread");
+        t.start(); 
+    }
+
+    public void stopThread() {
+    
+    }
+    
+    public void setGamePrep(GamePrep gamePrep) {
+        this.gamePrep = gamePrep;
+    }
+    
 
     public Rectangle getRectangle() {
         return r;
@@ -52,43 +75,31 @@ public class Logs extends Sprite implements Runnable {
             }
         }
         logLabel.setLocation(x, y); 
+        r.setLocation(x, y);
     }
 
     public JLabel getLogLabel() {
         return logLabel;
     }
     
-    
-    public void detectCollision(Character1 character) {
-        if (!gameStarted) {
-            return;
-        }
-
-        Rectangle characterRect = character.getRectangle();
-        boolean isOnLog = false;
-
-      if (r.intersects(characterRect)) {
-            if (characterRect.y + characterRect.height <= r.y + r.height && characterRect.y + characterRect.height >= r.y) {
-            	character.setY(r.y - characterRect.height);
-            	character.setX(character.getX() + speed);  
-                isOnLog = true;
-            }
-        }
-
-       if (!isOnLog) {
-            character.endGame(false);
-        }
+    public void detectCollision() {
+    	if (this.r.intersects(character1.getRectangle())) {
+    		
+    		character1.setX(x);
+    		character1Label.setLocation(this.x , this.y);  
+    	}
+    	
     }
-    
-    
-    
-    
     @Override
     public void run() {
         while (true) {
             try {
                 move(); 
-                Thread.sleep(50); 
+               detectCollision();
+               if (character1.isOnLog()) {
+                   character1Label.setLocation(character1.getX(), character1.getY());
+               }
+                Thread.sleep(100); 
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
