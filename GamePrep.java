86c7be5f729235e250f557b5196d9c5b3b9789ea
CianaTerese <W0402146@campus.nscc.ca>
2024-11-20 10:18:38 -0400

    Update GamePrep.java

--- GamePrep.java
+++ GamePrep.java
@@ -1,4 +1,3 @@
-
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Font;
@@ -14,6 +13,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
+
 public class GamePrep extends JFrame implements KeyListener, ActionListener {
     
     /**
@@ -24,14 +24,26 @@ public class GamePrep extends JFrame implements KeyListener, ActionListener {
 	 5.) Getting background to work and getting billie.png to get shown above background
 	 6.) Getting Billie.png finally to show and test collision 
 	 7.) somehow broke my bill :( 
+	
+	
+	after first look with darren
+	1.)fixed billie (and her rectangle because it wasnt moving and its x,y are 0) 
+	2.)fixed threads
+	3.)fixed connections between gameprep, sprite, logs, cars
+	4.)fixed cars collision
+	5.)fixed logs 
+	6.)made a water bounds so if she goes in the water she dies
+	7.)made the top space saying you win and get points
+	
+	 
 	 * 
 	 */
 
 	
 	
-	//Characters
+	//Character
 	private Character1 character1;
-    private Character2 character2;
+  
 
     //Logs & Cars
     private Logs[] logs;
@@ -39,8 +51,9 @@ public class GamePrep extends JFrame implements KeyListener, ActionListener {
 
     //GUI variables
     private Container content;
-    private JLabel character1Label, character2Label; 
-    private ImageIcon character1Image, character2Image;
+    private JLabel character1Label;
+    //CarsLabel; 
+    private ImageIcon character1Image;
     
     //2 buttons
     private JButton startBtn, visibilityBtn;
@@ -52,109 +65,85 @@ public class GamePrep extends JFrame implements KeyListener, ActionListener {
    //Game Data & Score
     private GameData gameData; 
     private JLabel scoreLabel;
-    
-    //Game Started
-    private boolean gameStarted = false; 
 
+    
     public GamePrep(String playername) {
         super("Billie Frogger");
-        gameData = new GameData(playername);
         
+        
+        //Screen
+        setSize(GameProperties.SCREEN_WIDTH, GameProperties.SCREEN_HEIGHT);
+        content = getContentPane();
+        content.setLayout(null);
+
+        //For Background
+        backgroundImage = new ImageIcon(getClass().getResource("/images/billiebackground.png")).getImage();  
+        //I used .getScaledInstance because it wasn't showing my background properly 
+        backgroundImage = backgroundImage.getScaledInstance(GameProperties.SCREEN_WIDTH, GameProperties.SCREEN_HEIGHT, Image.SCALE_SMOOTH);
+        //i changed the size of the images and moved the cars&logs
+        backgroundLabel = new JLabel(new ImageIcon(backgroundImage));
+        backgroundLabel.setSize(GameProperties.SCREEN_WIDTH, GameProperties.SCREEN_HEIGHT); 
+        backgroundLabel.setLocation(0, 0);
+        
+ 
+        //Game Data and Score Setup
+        gameData = new GameData(playername);
         scoreLabel = new JLabel("Score: " + gameData.getScore());
         scoreLabel.setFont(new Font("Arial", Font.BOLD, 24));
         scoreLabel.setForeground(Color.WHITE);
         scoreLabel.setBounds(10, 10, 200, 30);
        
         
-        //Characters
-        character1 = new Character1(460, 520, 50, 50, "Billie.png");
-        character2 = new Character2(0, 10, 45, 45, "BlueCar.png");
+        //Character1
+        character1 = new Character1(460, 520, 30, 30, "Billie.png");
+        character1Label = new JLabel();
+        character1Image = new ImageIcon(getClass().getResource("images/"+ character1.getImage()));
+        character1Label.setIcon(character1Image);
+        character1Label.setSize(character1Image.getIconWidth(), character1Image.getIconHeight());
         
-        //Background
-        //.getImage was because my background look good  otherwise
-        backgroundImage = new ImageIcon(getClass().getResource("/images/billiebackground.png")).getImage();
+        character1Label = character1.getCharacter1Label(); 
+        content.add(character1Label);
         
-        //I used .getScaledInstance because it wasn't showing my background properly 
-        backgroundImage = backgroundImage.getScaledInstance(GameProperties.SCREEN_WIDTH, GameProperties.SCREEN_HEIGHT, Image.SCALE_SMOOTH);
-        backgroundLabel = new JLabel(new ImageIcon(backgroundImage));
-        backgroundLabel.setSize(GameProperties.SCREEN_WIDTH, GameProperties.SCREEN_HEIGHT); 
-        backgroundLabel.setLocation(0, 0);
 
+        character1Label.setLocation(character1.getX(), character1.getY());
+        add(character1Label);
+        
         //Logs
         logs = new Logs[5];
         logs[0] = new Logs(0, 40, 50, 55, "log.png", 3, false); 
         logs[1] = new Logs(0, 80, 50, 55, "log.png", 2, true);  
         logs[2] = new Logs(0, 120, 50, 55, "log.png", 1, false); 
-        logs[3] = new Logs(0, 170, 50, 55, "log.png", 3, true);  
-        logs[4] = new Logs(0, 220, 50, 55, "log.png", 4, false);  
-       
-        //Log Thread
-        for (Logs log : logs) {
-            Thread logThread = new Thread(log);
-            logThread.start();
-            log.detectCollision(character1);
-        }
+        logs[3] = new Logs(0, 160, 50, 55, "log.png", 2, true);  
+        logs[4] = new Logs(0, 200, 50, 55, "log.png", 3, false);  
         
-        //Cars
+	  //Cars
         cars = new Cars[4];
-        cars[0] = new Cars(1000, 320, 45, 45, "BlueCar.png", 3, true);
-        cars[1] = new Cars(1200, 370, 45, 45, "BlueCar.png", 2, false);
-        cars[2] = new Cars(1100, 420, 45, 45, "BlueCar.png", 4, true);
-        cars[3] = new Cars(1300, 455, 45, 45, "BlueCar.png", 5, false);
-        //i took this car out so it wont hit bill instantly
-        //cars[4] = new Cars(1500, 410 + 100, 50, 50, "BlueCar.png", 1, true);
+        cars[0] = new Cars(850, 320, 50, 55, "BlueCar.png", 9, true);
+        cars[1] = new Cars(850, 380, 50, 55, "BlueCar.png", 5, false);
+        cars[2] = new Cars(850, 420, 50, 55, "BlueCar.png", 7, true);
+        cars[3] = new Cars(850, 460 , 50, 55, "BlueCar.png", 6, false);
         
-        //Car Thread
         for (Cars car : cars) {
-            Thread carThread = new Thread(car); 
-            carThread.start(); 
-        }
-        
-        
-        //For Background
-        setSize(GameProperties.SCREEN_WIDTH, GameProperties.SCREEN_HEIGHT);
-        content = getContentPane();
-        content.setLayout(null);
+        	car.setCharacter1(character1);
+      	car.setStartButton(startBtn); 
+            car.setVisibilityButton(visibilityBtn);
+            car.setGamePrep(this);
+            JLabel carLabel = car.getcarLabel();
+            content.add(carLabel);
+        }     
       
-        //Character 1
-        character1Label = new JLabel();
-        character1Image = new ImageIcon(getClass().getResource("images/"+ character1.getImage()));
-        character1Label.setIcon(character1Image);
-        character1Label.setLocation(character1.getX(), character1.getY());  
-        //got Bill to show
-        character1Label.setSize(character1Image.getIconWidth(), character1Image.getIconHeight());
-        
-       
-
-        character2Label = new JLabel();
-        character2Image = new ImageIcon(getClass().getResource("images/" + character2.getImage()));
-        character2Label.setIcon(character2Image);
-        character2Label.setSize(character2.getWidth(), character2.getHeight());
-        character2Label.setLocation(character2.getX(), character2.getY());
-
-        character2.setCharacter1(character1);
-        character2.setCharacter1Label(character1Label);
-        
-        character2.setCharacter2Label(character2Label);
-        
-        //Logs and Cars
-        for (Cars car : cars) {
-            content.add(car.getCarLabel());
-            
-        }
-
         for (Logs log : logs) {
-            content.add(log.getLogLabel());
+        log.setCharacter1(character1);
+        log.setGamePrep(this);
+        content.add(log.getLogLabel());
         }
-       
+        
         //Visibility Button
         visibilityBtn = new JButton("Hide");
         visibilityBtn.setSize(100, 50);
         visibilityBtn.setLocation(GameProperties.SCREEN_WIDTH - 100, GameProperties.SCREEN_HEIGHT - 100);
         visibilityBtn.setFocusable(false);
         visibilityBtn.addActionListener(this);
-        character2.setVisibilityButton(visibilityBtn);
-
         
         //StartButton
         startBtn = new JButton("Run");
@@ -162,30 +151,22 @@ public class GamePrep extends JFrame implements KeyListener, ActionListener {
         startBtn.setLocation(GameProperties.SCREEN_WIDTH - 100, GameProperties.SCREEN_HEIGHT - 200);
         startBtn.setFocusable(false);
         startBtn.addActionListener(this);
-        
-        
-        character2.setStartButton(startBtn);
 
         //Adding
         add(startBtn);
-        add(visibilityBtn);
-        content.add(character1Label);
-        content.add(character2Label);
-        content.add(scoreLabel);
-        content.add(backgroundLabel);
-        
-       
-        
+        add(visibilityBtn);  
+        add(scoreLabel);
+        add(backgroundLabel);
+     
         content.addKeyListener(this);
         content.setFocusable(true);
-      
-        
     }
-    
+
     
     //End Game
     public void endGame(boolean successful) {
-       
+       //test/
+    	System.out.println("End game triggered");
 
         String message = successful ? "Congratulations! You won!" : "Game Over! You lost!";
         JOptionPane.showMessageDialog(this, message, "Game Over", JOptionPane.INFORMATION_MESSAGE);
@@ -207,19 +188,32 @@ public class GamePrep extends JFrame implements KeyListener, ActionListener {
         }
     }
 
-    
-    
-    
-    
     //Restart Game
     public void restartGame() {
         gameData = new GameData(gameData.getPlayerName()); 
         scoreLabel.setText("Score: " + gameData.getScore()); 
+        //character1 = new Character1(460, 520, 30, 30, "Billie.png");
+        character1.setX(460);
+        character1.setY(520);
+        character1Label.setLocation(character1.getX(), character1.getY());
         
-        character1 = new Character1(460, 520, 50, 50, "Billie.png");
-        character2.setX(100);
-        character2.setY(100);
+        for (Cars car : cars) {
+            car.setX(car.getX());  
+            car.setY(car.getY());
+            car.stopThread();  
+        }
+        for (Logs log : logs) {
+            log.stopThread();  
+        }
 
+       
+        for (Cars car : cars) {
+            car.startThread();
+        }
+        for (Logs log : logs) {
+            new Thread(log).start(); 
+        }
+        
         this.setVisible(true);
     }
 
@@ -233,44 +227,64 @@ public class GamePrep extends JFrame implements KeyListener, ActionListener {
         int x = character1.getX();
         int y = character1.getY();
         
-        //Moving Up
-        if (e.getKeyCode() == KeyEvent.VK_UP){
+		if (e.getKeyCode() == KeyEvent.VK_UP){
 			y -= GameProperties.CHARACTER_STEP;
+			
+			
 			if( y + character1.getHeight() <= 0) {
 				y = GameProperties.SCREEN_HEIGHT;
+				endGame(true);
+				System.out.println("up");
 			}
-			
-			
-		//Moving Down
+
 		} else if (e.getKeyCode() == KeyEvent.VK_DOWN){
 			y += GameProperties.CHARACTER_STEP;
 			
-			if( y >= character1.getHeight ()) {
-				y = -1 * character1.getHeight();
+			if( y + character1.getHeight () > GameProperties.SCREEN_HEIGHT) {
+				//y = -1 * character1.getHeight();
+				y = GameProperties.SCREEN_HEIGHT - character1.getHeight();
+				System.out.println("down");
 			}
-		
-		//Moving Left
+
 		} else if (e.getKeyCode() == KeyEvent.VK_LEFT){
 			x -= GameProperties.CHARACTER_STEP;
 			
 			if (x + character1.getWidth() <= 0) {
 				x = GameProperties.SCREEN_WIDTH;
+				System.out.println("left");
 			}
 			
-		//Moving Right
+
 		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT){
 			x += GameProperties.CHARACTER_STEP;
 			
 			if ( x >= GameProperties.SCREEN_WIDTH) {
 				x = -1 * character1.getWidth();
-        }}
+				System.out.println("right");
+			}
 
-        
+		}
         
         character1.setX(x);
         character1.setY(y);
+      
         character1Label.setLocation(character1.getX(), character1.getY()); 
-        
+ 
+        boolean isOnLog = false;
+        for (Logs log : logs) {
+            if (log.getRectangle().intersects(character1.getRectangle())) {
+                isOnLog = true;
+                break;
+            }
+        }
+
+        if (isOnLog) {
+            System.out.println("Billie is on the log!");
+        } else if (character1.getY() >= 30 && character1.getY() <= 240) {
+            System.out.println("Billie is in the water!");
+            endGame(false);  
+            return;
+        }
    
     }
 
@@ -280,19 +294,29 @@ public class GamePrep extends JFrame implements KeyListener, ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == visibilityBtn) {
-            if (character2.getVisible()) {
-                character2.hide();
+        	for (Cars car: cars)
+            if (car.getVisible()) {
+                car.hide();
             } else {
-                character2.show();
+                car.show();
             }
         } else if (e.getSource() == startBtn) {
-            if (character2.getMoving()) {
-                character2.stopThread();
+        	for (Cars car: cars)
+            if (car.getMoving()) {
+                car.stopThread();
+                for (Logs log : logs) {
+                    log.stopThread();
+                }
             } else {
-                character2.startThread();
+                car.startThread();
+               
+                for (Logs log : logs) {
+                    new Thread(log).start();  
+                }
             }
         }
     }
+    
 
     public static void main(String[] args) {
     	   String playerName = JOptionPane.showInputDialog("Enter your name:");
@@ -304,16 +328,4 @@ public class GamePrep extends JFrame implements KeyListener, ActionListener {
     	    GamePrep myGame = new GamePrep(playerName); 
     	    myGame.setVisible(true); 
     }
-
-
-	public boolean isGameStarted() {
-		return gameStarted;
-	}
-
-
-	public void setGameStarted(boolean gameStarted) {
-		this.gameStarted = gameStarted;
-	}}
-
-
-
+}
